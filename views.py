4f61442ebca9323f237e310bb78906188b9d364a
Катаев Денис <bteamko+git@gmail.com>
2011-10-06 13:21:57 +0600

    Переписал главную функцию

--- views.py
+++ views.py
@@ -3,11 +3,12 @@ from django.utils import simplejson
 from django.http import HttpResponse
 from django.shortcuts import render
 from settings import PROJECT_PATH
-import os
+from subprocess import call
+import datetime
 
 class UploadFileForm(forms.Form):
     head = forms.CharField(max_length=500,required=False)
-    body = forms.CharField(max_length=50,required=False)
+    text = forms.CharField(max_length=50,required=False)
     file = forms.FileField()
 
 
@@ -19,15 +20,28 @@ def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            f = open(STATIC_ROOT+'img/test1.jpg','wb+')
+            f = open(STATIC_ROOT+'img/%s.jpg' % datetime.datetime.now().isoformat(),'wb+')
             for chunk in request.FILES['file'].chunks():
                 f.write(chunk)
             f.close()
-            os.popen(u"bash dem.sh -i %s -o %s -h '%s' -t '%s'" % (f.name,f.name+'.jpg',
-                                                                   unicode(form.cleaned_data.get('head')),
-                unicode(form.cleaned_data.get('body'))))
+            head = form.cleaned_data.get('head')
+            text = form.cleaned_data.get('text')
+            font = "/home/bteam/funny_pic/fonts/PT_Sans/PT_Sans-Web-Bold.ttf"
+            out = f.name+'out.jpg'
+            call(['convert','-scale','640',f.name,out])
+            call(['mogrify', '-bordercolor','black','-border','2','-bordercolor','white',
+                  '-border','2','-bordercolor','black','-border','70x0',out])
+            if head: call(['montage','-geometry','+0+0','-background','black','-fill','white',
+                  '-font',font,'-pointsize', '64',
+                  '-label',head,out,out])
+            if text: call(['montage','-geometry','+0+0','-background','black','-fill','white',
+                  '-font',font,'-pointsize', '32',
+                  '-label',text,out,out])
+            if head or text: call(['mogrify','-bordercolor','black','-border','5x45',out])
+            call(['convert',out,out])
+
         return HttpResponse("<textarea>%s</textarea>" % simplejson.dumps({'success':form.is_valid(),
-               'value':1,'errors':form.errors,'img':f.name.split('/')[-1]}))
+               'value':1,'errors':form.errors,'img':out.split('/')[-1]}))
     else:
 
         form = UploadFileForm()
