d49f57d77d371ea043d0e40a0d608ab8aa5d87e8
Катаев Денис <bteamko+git@gmail.com>
2011-10-06 10:41:56 +0600

    Черновой вариант

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': 'funny',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -72,6 +72,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH,'static/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- views.py
+++ views.py
@@ -2,20 +2,33 @@ from django import forms
 from django.utils import simplejson
 from django.http import HttpResponse
 from django.shortcuts import render
+from settings import PROJECT_PATH
+import os
 
 class UploadFileForm(forms.Form):
-#    title = forms.CharField(max_length=50)
+    head = forms.CharField(max_length=500,required=False)
+    body = forms.CharField(max_length=50,required=False)
     file = forms.FileField()
 
 
 def upload_file(request):
+    """
+
+    """
+    STATIC_ROOT = PROJECT_PATH + '/static/'
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            f = open('/home/bteam/test.jpg','wb+')
+            f = open(STATIC_ROOT+'img/test1.jpg','wb+')
             for chunk in request.FILES['file'].chunks():
                 f.write(chunk)
-        return HttpResponse("<textarea>%s</textarea>" % simplejson.dumps({'success':form.is_valid(),'value':1,'errors':form.errors}))
+            f.close()
+            os.popen(u"bash dem.sh -i %s -o %s -h '%s' -t '%s'" % (f.name,f.name+'.jpg',
+                                                                   unicode(form.cleaned_data.get('head')),
+                unicode(form.cleaned_data.get('body'))))
+        return HttpResponse("<textarea>%s</textarea>" % simplejson.dumps({'success':form.is_valid(),
+               'value':1,'errors':form.errors,'img':f.name.split('/')[-1]}))
     else:
+
         form = UploadFileForm()
     return render(request,'upload.html',{'form':form})
